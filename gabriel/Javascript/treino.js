842a713d1c61672f9e330a8f3613249d14253e76
Fernandes <gf13368@gmail.com>
2024-11-08 23:39:12 -0300

    Exerc. javascript

---
+++ gabriel/Javascript/treino.js
@@ -0,0 +1,35 @@
+ /* 1 -
+const nome = 'Gabriel' 
+var idade = 21 
+const frase = 'Olá, meu nome é ' + nome + ' e tenho ' + idade + ' anos'
+
+// resultado questão 1
+console.log('EXERCICIO 1: \n', frase) 
+*/
+
+
+/* 2 - Condições: Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade. 
+const idade = prompt("Digite sua idade: ")
+console.log('EXERCICIO 2: \n')
+if (idade<=17) {
+  console.log("Menor de idade, dê meia volta")
+} else {
+  console.log("Maior de idade, pode entrar")
+} */
+
+/* 3 - Loops: Crie um loop que imprima todos os números de 1 a 100.
+var count;
+for (count=0; count <= 100; count++) {
+  console.log(count)
+} */
+
+/*4 - Funções: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+
+var a = 30;
+var b = 7;
+
+function soma(a, b) {
+    return a+b;
+}
+
+console.log(soma) */
