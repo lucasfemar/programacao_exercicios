f211cdff8a26d86c9478d65c46e9624c4b0801e5
lucasfemar <lucasdeveloper98l@gmail.com>
2024-11-14 21:15:46 -0300

    Add indexjs na pasta

---
+++ VMarcelo/index.js
@@ -0,0 +1,28 @@
+// STRING
+var variavel1 = 'Ola'
+let variavel2 = 'Tudo bem?'
+console.log('Meu cumprimento é: ' + variavel1 + ', ' + variavel2)
+console.log(`Meu cumprimento é: ${variavel1}, ${variavel2}`)
+
+// NUMBER
+let a = 10;
+const b = 26;
+console.log(a + b);
+
+// Boolean
+const valorVerdadeiro = true;
+const valorFalso = false;
+console.log(valorVerdadeiro);
+console.log(valorFalso);
+
+// Object -> // array
+const pessoa = {
+    nome: 'lucas',
+    idade: 26
+}
+const frutas = ['abacaxi', 3, 'pera']
+console.log(typeof pessoa)
+console.log(Array.isArray(pessoa))
+console.log(Array.isArray(frutas))
+
+// Trazer - o que é função, loops e condionais ***array
