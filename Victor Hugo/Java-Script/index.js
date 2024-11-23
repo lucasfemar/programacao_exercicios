87eac69065310424e4b91fe214aad87a289ac9ae
Hugo325 <victoramaralhugo523@gmail.com>
2024-11-23 09:20:16 -0300

    teste

---
+++ Victor Hugo/Java-Script/index.js
@@ -0,0 +1,35 @@
+//  exercicio1
+const nome = "João";
+const idade = 25;
+console.log("Olá, meu nome é" +nome+ "e eu tenho" +idade+ "anos");
+
+/*Exercicio2
+  
+    let idade = prompt("Qual a sua idade?");
+  if (idade >= 18) {
+   console.log("Você é maior de idade, Autorização válida.");
+  } else {
+ console.log("Você é menor de idade, Autorização inválida.");
+}
+
+*/
+
+/* Exercicio4
+
+function soma(num1, num2) {
+    return num1 + num2;
+}
+console.log('O Resultado é ' + soma(5, 10));
+*/
+
+
+/* Exercicio 5
+
+function (n) {
+    if (n % 2 === 0) {
+}
+    console.log(n, 'Número par);
+} else {}
+    console.log(n, 'Número Impar); 
+}
+*/
