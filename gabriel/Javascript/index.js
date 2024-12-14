2ec044d461c6bbf4d432a238809fc5d6c5c8bd03
Fernandes <gf13368@gmail.com>
2024-12-14 08:23:57 -0300

    js - manipulação de objeto

---
+++ gabriel/Javascript/index.js
@@ -0,0 +1,13 @@
+/**const car = {
+    brand: 'Toyota',
+    model: 'AE86',
+    year: 1986,
+    color: 'white'
+  }
+  
+  console.log(car)
+  
+  const troca = car;
+  troca.color = 'blue';
+  console.log(car.color)*/
+
