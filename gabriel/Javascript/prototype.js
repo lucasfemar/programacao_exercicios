6117cf5a5e9ee68a8fb52dccc1b10ccf9a3db709
Fernandes <gf13368@gmail.com>
2025-01-12 23:21:41 -0300

    att de veiculo e estudante no prot.

--- gabriel/Javascript/prototype.js
+++ gabriel/Javascript/prototype.js
@@ -1,4 +1,7 @@
+// Exercicio 1
 const veiculo = {
+    tipo: "veiculo generico",
+    rodas: 4,
     descrever: function() {
         console.log(`Esse é um ${this.tipo}`);
     }
@@ -10,3 +13,19 @@ const carro = {
 }
 
 Object.setPrototypeOf(carro, veiculo);
+
+carro.descrever()
+console.log(carro.rodas)
+
+
+// Exercicio 2
+const pessoa = {
+    nome: 'Nome Padrão',
+    cumprimento: function() {
+        console.log(`Olá, meu nome é ${this.nome}`)
+    }
+}
+
+const estudante = Object.create(pessoa);
+estudante.nome = 'Alice';
+estudante.cumprimento(); //4 - será exibido a função cumprimento com o nome Alice
