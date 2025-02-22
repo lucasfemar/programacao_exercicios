0642a6af7a49575116d30478b6b8b55857249537
Fernandes <gf13368@gmail.com>
2025-02-22 08:45:54 -0300

    exercicios 1 e 2

---
+++ gabriel/Javascript/exercicios/exer_1.js
@@ -0,0 +1,7 @@
+//fumção para somar 2 valores distintos que são passados para o paramentro
+function somar(a, b) {
+    return a + b;
+}
+
+//pega a função somar e os valores dela e retorna o valor final
+console.log(somar(4, 7));
---
+++ gabriel/Javascript/exercicios/exer_2.js
@@ -0,0 +1,9 @@
+pessoa = {
+    nome: "Gabriel",
+    idade: 21,
+    cidade: "SP"
+}
+
+pessoa.nova_cidade = "Minas Gerais"
+
+console.log(pessoa)
