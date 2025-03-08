66a81a711745d9c3702abe8282ebd0ad673882b0
Victortw4 <victortw4@gmail.com>
2025-03-08 07:54:10 -0300

    exercicios

---
+++ victorR/exercicio2.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Victor",
+    idade: 15,
+    Cidade: "São Paulo",
+}
+
+function alterarCidade(novaCidade) {
+    console.log("Cidade antiga:", pessoa.Cidade);
+    pessoa.Cidade = novaCidade;
+    console.log("Nova Cidade:", pessoa.Cidade)
+} 
+
+alterarCidade("Rio de janeiro");
---
+++ victorR/exercicio3.js
@@ -0,0 +1,8 @@
+function tabuada(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${n} x ${i} = ${n * i}`);
+        
+    }
+}
+
+tabuada(1);
