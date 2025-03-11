c772f308bae4bc63c0903b63d28c2f6369d83276
daniele <daniele2098899@gmail.com>
2025-03-11 09:08:07 -0300

    exercicio tabuada

---
+++ daniele/exercicios/tabuada_personalizada.js
@@ -0,0 +1,7 @@
+function tabuadaPersonalizada (numero, inicio, fim){
+    //numero = minha tabuada
+    //inicio e = com qual numero minha tabuada começa
+    //com qual numero minha tabuada termina
+    //titúlo da tabuada
+
+}
