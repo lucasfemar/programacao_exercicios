0444c86095c1b75b3fe1a0bb151d1e53ded32dee
daniele <daniele2098899@gmail.com>
2025-05-27 12:06:01 -0300

    front-end/daniele

---
+++ daniele/calculadora/cal.js
@@ -0,0 +1,5 @@
+const previusOperationText = document.querySelector("#previous-operation")
+const currentOperationText = document.querySelector("#current-operation")
+const buttons = document.querySelectorAll("buttons-container button")
+
+
