00497a1359a61cb4cd14cc680bccc83ea3247555
Lucas Marcelo <65972119+lucasfemar@users.noreply.github.com>
2025-06-28 17:32:44 +0000

    add comentario para ajudar na leitura do código

--- Lucas/Formulario/Backend/server.js
+++ Lucas/Formulario/Backend/server.js
@@ -3,15 +3,15 @@ import cors from "cors";
 
 const server = express();
 
-server.use(cors());
+server.use(cors()); // Permitir o frontend acessar os servidor
 
-server.use(express.json());
+server.use(express.json()); // Dizer para o servidor que vamos trabalhar com dados em JSOn
 
-server.post("/user", (request, response) => {
+server.post("/user", (request, response) => { // Disponibilzia a rota /user com o metodo POST
   console.log(request.body);
   response.send();
 });
 
-server.listen("3000", () => {
+server.listen("3000", () => { // Servir o backend na porta 3000
   console.log("Listening on http://localhost:3000");
 });
